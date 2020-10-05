a3d26554020f8721f5046b454ad3bc9d3d5a8648
rewon123 <rewonrizwan123@gmail.com>
2020-10-06 03:48:54 +0600

    done finally marks ta aktu beshi diyen :_)))

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ client.connect(err => {
             })
     })
     app.get('/register/:id', (req, res) => {
-        eventsCollection.find({ key: req.params.key })
+        eventsCollection.find({_id: ObjectId(req.params.id)})
             .toArray((err, documents) => {
                 res.send(documents[0])
             })
